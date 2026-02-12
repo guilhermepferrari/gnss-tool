88e36529ea154d5f2c6c9b6dbbb299bfa011a23c
vpferrari <guilhermepferrari@gmail.com>
2026-02-11 21:15:02 -0300

    rinex reboot

--- include/rinex.h
+++ include/rinex.h
@@ -4,60 +4,11 @@
 
 /* constants -----------------------------------------------------------------*/
 
-
-#define MAX_OBS_TYPES 16	/* max # of obs values per sat (C1, L1, etc) */
-#define MAX_SATS      64	/* max # of satellites in a single obs epoch */
-#define MAX_EPH       64    /* max # of ephemeris records stored */
-
+// TODO: definitions
 
 /* type definitions ----------------------------------------------------------*/
 
-
-typedef struct {	/* calendar timestamp */
-	int year, month, day, hour, minute;
-	double sec;
-} gps_time_t; // GpsTime
-
-
-typedef struct {	/* gnss satellite measurements for a single epoch */
-	char id[4];     /* examples: "G01", "R12"*/
-	double obs[MAX_OBS_TYPES]; /* observation rules */
-} sat_obs_t; // SatObs
-
-
-typedef struct {	/* all sat observations belonging to the same epoch */
-	gps_time_t time;
-	int n_sat;
-	sat_obs_t sats[MAX_SATS]
-} obs_epoch_t; // ObsEpoch
-
-
-typedef struct {	/* obs header metadata */
-	double version; // rinex version number
-	int n_obs; // number of observation types
-	char obs_types[MAX_OBS_TYPES][4]; // observation type indentifiers
-	double interval; // time space between epochs, in seconds
-} rinex_obs_header;
-
-
-typedef struct {	/* nav ephemeris record */
-	char sat_id[4]; // satellite identifier
-	double af0, af1, af2; // satellite clock correction coefficients
-	double toe; // reference time (time of ephemeris)
-	double sqrt_a; // square root of semi-major axis
-	double e; // orbital eccentricity
-	double i0; // inclination angle at reference time
-	double omega_0; // longitude of ascending mode 
-	double omega; // argument of perigee
-	double m0; // mean anomaly at reference time
-	double omega_dot; // rate of right ascencion
-	double idot; // rate of inclination
-} ephemeris;
-
-typedef struct {
-	int n;
-	ephemeris eph[MAX_EPH];
-} nav_store;
+// TODO: new structs
 
 /* function prototypes -------------------------------------------------------*/
 
@@ -72,20 +23,12 @@ int cmd_rinex(int argc, char *argv[]);
 void print_rinex_usage();
 
 /**
- * @brief Reads and parses RINEX observation headers.
+ * @brief Reads and parses RINEX observation files.
  * 
- * @param[in] fp Pointer to the observation RINEX file.
- * 
- * @param[in] hdr Pointer to the header variable that will hold
- *                the parsed header values from the file.
- * 
- * @return None. This function will update the hdr variable with
- *               header values from fp RINEX file.
+ * @param[in] filepath Path to RINEX file.
+ *  
+ * @return Observation data structure containing both headers and observation
  */
-int read_obs_header(FILE *fp, rinex_obs_header *hdr);
+int read_obs_header(char filepath);
 
-/**
- *@brief TODO: document read_obs_epoch
- */
-int read_obs_epoch(FILE *fp, const rinex_obs_header *hdr, obs_epoch_t *epoch);
 #endif
--- src/rinex.c
+++ src/rinex.c
@@ -23,35 +23,13 @@ void print_rinex_usage(){
 /* rinex observation parser functions ....................................... */
 
 
-int read_obs_header(FILE *fp, rinex_obs_header *hdr) {
-	char line[128];					// fixed line width
+int read_obs_file(filepath) {
+/* 1. read file ..............................................................*/
+/* 2. iterate each line of the file ..........................................*/
+/* 2.1 parse header ..........................................................*/
+/**/
 
-	while (fgets(line, sizeof(line), fp)) {
-
-		if (strstr(line, "RINEX VERSION")) {	// substring for version
-			hdr->version = atof(line);	// convert to double
-		}
-
-		if (strstr(line, "# / TYPES OF OBSERV")) { // substr for obs_t
-			sscanf(line,"%d", &hdr->n_obs); // scan and store
-			for (int i = 0; i < hdr->n_obs; i++)
-				strncpy(hdr->obs_types[i], line + 10 + i*6, 3);
-		}
-
-		if (strstr(line, "INTERVAL")) {		// substr for interval
-			hdr->interval = atof(line);
-		}
-
-		if (strstr(line, "END OF HEADER")) {
-			return 1;
-		}
-	}
-	return 0;
 }
-
-// TODO: read_obs_epoch
-
-
 
 
 
